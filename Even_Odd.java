3b0a90f781ca65fea66593ebe829aef4937d940e
Aviansh Udata <60933551+avinashudata52@users.noreply.github.com>
2024-07-16 14:02:03 +0530

    Create Even_Odd.java
    v2

---
+++ Even_Odd.java
@@ -0,0 +1,21 @@
+class Even_Odd
+{  
+  public static void main(String args[])
+{
+    int n = 131;
+    if(n%2==0)
+    {
+      System.out.pritln(n+" This is Even Number");
+    }
+    else
+    {
+      System.out.println(n+ " this is Odd Number");
+    }
+}
+
+
+
+
+
+
+}
