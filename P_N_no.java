6f3ee85d8a0960b52ec46da36fea95ee7c5f0c9f
Aviansh Udata <60933551+avinashudata52@users.noreply.github.com>
2024-07-16 13:54:27 +0530

    Add files via upload

---
+++ P_N_no.java
@@ -0,0 +1,15 @@
+class P_N_no
+{
+	public static void main(String args[])
+	{
+		int num=11;
+		if(num>0)
+		{
+		System.out.println("This is Positive Number");
+		}
+		else if(num < 0)
+		{
+		System.out.println("This is Negetive number");
+		}
+	}
+}
