5d2566105e0e186a58e3052a64356b555e8fb15f
Aviansh Udata <60933551+avinashudata52@users.noreply.github.com>
2024-07-17 20:51:21 +0530

    Sum_of_Integer.java
    fourth commit

---
+++ Sum_of_Integer.java
@@ -0,0 +1,15 @@
+class Sum_of_Integer
+{
+    public static void main(String args[])
+    {
+        int a = 5; 
+        int b = 15;
+
+        int sum = 0;
+        for(int i=a; i<=b; i++)
+        {
+            sum = sum+1;
+          System.out.println(sum);
+        }
+    }
+}
