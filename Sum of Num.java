1ffb77a9a9229f31e6fedf32dfbe6b7021efa677
Aviansh Udata <60933551+avinashudata52@users.noreply.github.com>
2024-07-17 20:40:04 +0530

    Create Sum of Num.java
    third commit

---
+++ Sum of Num.java
@@ -0,0 +1,14 @@
+class Sum of Num
+{
+    public static void main(String args[])
+    {
+        int n = 10;
+        int sum = 0;
+
+      for(int i=1; i<n; i++)
+      {
+        sum +=i;
+      System.out.println(sum);
+      }
+    }
+}
