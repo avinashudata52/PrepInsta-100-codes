77213fffd5851aab4d3a1678052e1f583afb6a11
Aviansh Udata <60933551+avinashudata52@users.noreply.github.com>
2024-07-17 21:14:41 +0530

    Greatest_of_the_Three_number.java
    fith commit

---
+++ Greatest_of_the_Three_number.java
@@ -0,0 +1,21 @@
+class Greatest_of_the_Three_number
+{
+      public static void main(String args[])
+      {
+          int n1 = 10, n2 = 20. n3=30;
+
+          if(n1>=n2 && n1>=n3)
+          {
+              System.out.println(n1+"This no is greater");
+          }
+          else if(n2>=n1 && n2>=n3)
+          {
+              System.out.println(n2+"This no is greater");
+          }
+          else
+          {
+                System.out.println(n3+"This no is greater");
+          }
+      }
+
+}
